b51a90e3ce01b4176cc2f93c1d1ddf7954b4069c
Ali Azouz <132236484+Azouz8@users.noreply.github.com>
2024-10-05 02:55:21 +0300

    Add Nav Bar Item and Navigation Between Them

--- app/src/main/java/com/example/habittrackerapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/habittrackerapp/ui/screens/HomeScreen.kt
@@ -1,18 +1,30 @@
 package com.example.habittrackerapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun HomeScreen(navController: NavHostController){
-    Text(text = "Home Screen")
+fun HomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home Screen")
+    }
 }
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun HomeScreenPreview(){
-    HomeScreen( rememberNavController())
+fun HomeScreenPreview() {
+    HomeScreen()
 }
 
