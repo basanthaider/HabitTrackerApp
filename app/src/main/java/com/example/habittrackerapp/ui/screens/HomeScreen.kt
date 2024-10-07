508d988a6f4007193a3218b435693ea781e3d69d
Ali Azouz <132236484+Azouz8@users.noreply.github.com>
2024-10-07 19:17:20 +0300

    Modify the calendar view

--- app/src/main/java/com/example/habittrackerapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/habittrackerapp/ui/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.habittrackerapp.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -20,10 +23,16 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.habittrackerapp.utils.CalendarView
+import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
+import com.maxkeppeler.sheets.calendar.CalendarDialog
+import com.maxkeppeler.sheets.calendar.models.CalendarConfig
+import com.maxkeppeler.sheets.calendar.models.CalendarSelection
+import com.maxkeppeler.sheets.calendar.models.CalendarStyle
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavHostController) {
+    val calendarState = rememberUseCaseState()
     Scaffold { innerPadding ->
         Column {
             Text(
@@ -31,17 +40,28 @@ fun HomeScreen(navController: NavHostController) {
                 fontSize = 24.sp,
                 modifier = Modifier.padding(start = 16.dp, top = 16.dp)
             )
+            CalendarDialog(
+                state = calendarState,
+                selection = CalendarSelection.Date { date ->
+                    Log.d("trace", "Selected date: ${date.dayOfWeek}")
+                },
+                config = CalendarConfig(
+                    style = CalendarStyle.WEEK,
+                ),
+            )
             Row(
                 verticalAlignment = Alignment.Bottom
             ) {
-                CalendarView()
                 Box(
                     modifier = Modifier
                         .padding(innerPadding)
                         .fillMaxSize()
                 ) {
+                    //Add Habit
                     FloatingActionButton(
-                        onClick = {},
+                        onClick = {
+                            navController.navigate("/addHabit")
+                        },
                         modifier = Modifier
                             .align(Alignment.BottomEnd)
                             .padding(end = 16.dp),
@@ -49,6 +69,19 @@ fun HomeScreen(navController: NavHostController) {
                         contentColor = Color.White
                     ) {
                         Icon(Icons.Filled.Add, contentDescription = "Add")
+                    }
+                    // Calendar
+                    FloatingActionButton(
+                        onClick = {
+                            calendarState.show()
+                        },
+                        modifier = Modifier
+                            .align(Alignment.BottomStart)
+                            .padding(start = 16.dp),
+                        containerColor = Color(0xff485d92),
+                        contentColor = Color.White
+                    ) {
+                        Icon(Icons.Filled.DateRange, contentDescription = "Calendar")
                     }
                 }
             }
