7d950a27566eea91aee7bbc3f10efc945e8f9c26
BaSant <basssnthaider@gamil.com>
2024-10-08 04:37:13 +0300

    finishing the statistics screen

--- app/src/main/java/com/example/habittrackerapp/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/habittrackerapp/ui/screens/ProfileScreen.kt
@@ -1,58 +1,383 @@
 package com.example.habittrackerapp.ui.screens
 
+import android.view.LayoutInflater
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-
-
+import com.example.habittrackerapp.R
+import com.example.habittrackerapp.ui.theme.Blue
+import com.example.habittrackerapp.ui.theme.DarkBlue
+import com.example.habittrackerapp.utils.Utils
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 
 @Composable
-fun ProfileScreen(navController: NavHostController){
+fun ProfileScreen(navController: NavHostController) {
     Scaffold(topBar = {
-        Box (modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp),
+            contentAlignment = Alignment.Center,
         )
         {
-
-
         }
     })
     {
-        Column (
+        Column(
             modifier = Modifier.padding(it),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-            )
+        )
         {
-            Text(text = "Profile Screen")
+            LineChart()
+            StreakCard()
+            Grid()
 
         }
-
     }
 }
 
 
+@Composable
+fun LineChart() {
+    val context = LocalContext.current
+    val sampleData = listOf( // Replace with your actual data
+        Entry(1f, 10f),
+        Entry(2f, 15f),
+        Entry(3f, 20f),
+        Entry(4f, 18f),
+        Entry(5f, 25f)
+    )
+
+    AndroidView(
+        factory = {
+            val view = LayoutInflater.from(context).inflate(R.layout.line_chart, null)
+            view
+        }, modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+    )
+    { view ->
+        val lineChart = view.findViewById<LineChart>(R.id.lineChart)
+        val lineDataSet = LineDataSet(sampleData, "Habits").apply {
+            color = Blue.toArgb()
+            valueTextColor = DarkBlue.toArgb()
+            lineWidth = 3f
+            axisDependency = YAxis.AxisDependency.RIGHT
+            setDrawFilled(true)
+            mode = LineDataSet.Mode.CUBIC_BEZIER
+            valueTextSize = 12f
+            val drawable = ContextCompat.getDrawable(context, R.drawable.chart_gardient)
+            drawable?.let {
+                fillDrawable = it
+            }
+
+        }
+        lineChart.xAxis.valueFormatter = object :
+            com.github.mikephil.charting.formatter.ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                return Utils.formatDataForChart(value.toLong())
+            }
+        }
+        lineChart.data = LineData(lineDataSet)
+        lineChart.invalidate()
+        lineChart.axisLeft.isEnabled = false
+        lineChart.axisRight.isEnabled = false
+        lineChart.axisLeft.setDrawGridLines(false)
+        lineChart.axisRight.setDrawGridLines(false)
+        lineChart.xAxis.setDrawGridLines(false)
+        lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        lineChart.xAxis.setDrawAxisLine(false)
+    }
+}
+
 
 @Composable
-fun LineChart(){
+fun StreakCard() {
+    Card(
+        elevation = CardDefaults.cardElevation(),
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Blue
+        ),
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(top = 8.dp),
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(
+                    text = "0 Days",
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(
+                    text = "Your current streak",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(
+                    text = "0 Days",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+                Text(
+                    text = "your longest streak ",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.White,
+                )
+
+
+            }
+            Image(
+                painter = painterResource(id = R.drawable.trophy),
+                contentDescription = "streak",
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .size(100.dp)
+                    .weight(1f)
+            )
+
+        }
+    }
 
 }
+
+@Composable
+fun Grid() {
+    Column (  modifier = Modifier.padding(4.dp)){
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            Card(
+                elevation = CardDefaults.cardElevation(),
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.cardColors(containerColor = DarkBlue),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .weight(1f)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 4.dp),
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.calender_png),
+                            contentDescription = "streak",
+                            modifier = Modifier.size(50.dp)
+
+                        )
+                        Text(
+                            text = "0 Days",
+                            style = MaterialTheme.typography.headlineLarge,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(
+                            text = "Total perfect days",
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                    }
+
+                }
+            }
+            Card(
+                elevation = CardDefaults.cardElevation(),
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.cardColors(containerColor = DarkBlue),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .weight(1f)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 8.dp),
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.check),
+                            contentDescription = "streak",
+                            modifier = Modifier
+                                .size(50.dp)
+
+                        )
+                        Text(
+                            text = "0",
+                            style = MaterialTheme.typography.headlineLarge,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(
+                            text = "total completed habits",
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                    }
+
+                }
+            }
+
+        }
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            Card(
+                elevation = CardDefaults.cardElevation(),
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.cardColors(containerColor = DarkBlue),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .weight(1f)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 8.dp),
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.rate),
+                            contentDescription = "streak",
+                            modifier = Modifier.size(50.dp)
+
+                        )
+                        Text(
+                            text = "0 %",
+                            style = MaterialTheme.typography.headlineLarge,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(
+                            text = "Habit completion rate",
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                    }
+
+                }
+            }
+            Card(
+                elevation = CardDefaults.cardElevation(),
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.cardColors(containerColor = DarkBlue),
+                modifier = Modifier
+                    .padding(8.dp)
+                    .weight(1f)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 8.dp),
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.avarge),
+                            contentDescription = "streak",
+                            modifier = Modifier
+                                .size(50.dp)
+
+                        )
+                        Text(
+                            text = "0.0",
+                            style = MaterialTheme.typography.headlineLarge,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(
+                            text = "average per day",
+                            style = MaterialTheme.typography.bodySmall,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                    }
+
+                }
+            }
+
+        }
+    }
+    }
+
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun ProfileScreenPreview(){
-    ProfileScreen( rememberNavController())
+fun ProfileScreenPreview() {
+    ProfileScreen(rememberNavController())
 }
+
+/*@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun LineChartPreview(){
+    LineChart()
+}*/
+/*@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun StreakCardPreview(){
+    StreakCard()
+}*/
+
+
+/*@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun GridPreview(){
+    Grid()
+}*/
---
+++ app/src/main/java/com/example/habittrackerapp/utils/Utils.kt
@@ -0,0 +1,12 @@
+package com.example.habittrackerapp.utils
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+object Utils {
+    fun formatDataForChart(dataInMillies:Long):String{
+        val dataFormatter =SimpleDateFormat(("dd-MM"),Locale.getDefault())
+        return dataFormatter.format(dataInMillies)
+    }
+
+}
