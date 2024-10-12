67cec3e4bb733d8640a50f0e8403c8c9160e912a
BaSant <basssnthaider@gamil.com>
2024-10-12 06:19:38 +0300

    add Top app bar with icons and add setting screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
