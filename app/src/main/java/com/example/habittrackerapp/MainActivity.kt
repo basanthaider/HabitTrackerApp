f59c7504739a5b7ced62c452cf99789b3703c150
Mariam Essam <mariam.non30@gmail.com>
2024-10-20 17:59:15 +0300

    reformat code for readability

--- app/src/main/java/com/example/habittrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/habittrackerapp/MainActivity.kt
@@ -11,16 +11,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.res.painterResource
 import androidx.core.content.ContextCompat
 import com.example.habittrackerapp.repository.HabitViewModel
 import com.example.habittrackerapp.ui.screens.NavHostScreen
+import com.example.habittrackerapp.ui.screens.PermissionDeniedDialog
 import com.example.habittrackerapp.ui.theme.HabitTrackerAppTheme
 import com.google.firebase.FirebaseApp
 
@@ -34,16 +30,45 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        // Initialize Firebase
         FirebaseApp.initializeApp(this)
 
-        // Create an instance of HabitViewModel
         val habitViewModel = HabitViewModel()
 
-        // Create Notification Channel
         createNotificationChannel()
 
-        // Initialize notification permission launcher
+        initializeNotificationPermissionLauncher()
+
+        setContent {
+            HabitTrackerAppTheme {
+                // Main UI
+                NavHostScreen(
+                    habitViewModel = habitViewModel,
+                    onRequestNotificationPermission = {
+                        checkAndRequestNotificationPermission()
+                    }
+                )
+
+                // Show the dialog when permission is denied for the first time
+                handlePermissionDenied()
+            }
+        }
+    }
+
+    // This function checks if the permission is already granted
+    private fun checkAndRequestNotificationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is already granted, you can send a notification if needed
+        } else {
+            // If permission hasn't been requested yet, request it
+            notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
+
+    private fun initializeNotificationPermissionLauncher() {
         notificationPermissionLauncher = registerForActivityResult(
             ActivityResultContracts.RequestPermission()
         ) { isGranted ->
@@ -51,7 +76,6 @@ class MainActivity : ComponentActivity() {
                 // Permission granted; you can send a notification if needed
                 // Hide the dialog when permission is granted
                 showPermissionDeniedDialog.value = false
-                // sendNotification(this)
             } else {
                 // Check if the user has denied the permission before without selecting "Don't ask again"
                 val prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
@@ -68,85 +92,34 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-
-
-        setContent {
-            HabitTrackerAppTheme {
-                // Main UI
-                NavHostScreen(
-                    habitViewModel = habitViewModel,
-                    onRequestNotificationPermission = { checkAndRequestNotificationPermission() }
-                )
-
-                // Show the dialog when permission is denied for the first time
-                if (showPermissionDeniedDialog.value) {
-                    PermissionDeniedDialog(
-                        onActionClicked = { allowPressed ->
-                            if (allowPressed) {
-                                // User pressed "Allow"
-                                notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-                            } else {
-                                // Set the flag that permission was denied
-                                getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit()
-                                    .putBoolean(KEY_PERMISSION_DENIED_ONCE, true).apply()
-                                showPermissionDeniedDialog.value = false
-                                // Show the toast message on denial
-                                Toast.makeText(
-                                    this,
-                                    "You need to allow notifications to activate reminders!",
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
-                        }
-                    )
-                }
-            }
-        }
-    }
-
-    // This function checks if the permission is already granted
-    private fun checkAndRequestNotificationPermission() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.POST_NOTIFICATIONS
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            // Permission is already granted, you can send a notification if needed
-            // sendNotification(this)
-        } else {
-            // If permission hasn't been requested yet, request it
-            notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-        }
     }
 
     @Composable
-    private fun PermissionDeniedDialog(onActionClicked: (Boolean) -> Unit) {
-        AlertDialog(
-            onDismissRequest = { /* Do nothing */ },
-            confirmButton = {
-                TextButton(onClick = { onActionClicked(true) }) {
-                    Text(text = "Allow")
+    private fun handlePermissionDenied() {
+        if (showPermissionDeniedDialog.value) {
+            PermissionDeniedDialog(
+                onActionClicked = { allowPressed ->
+                    if (allowPressed) {
+                        // User pressed "Allow"
+                        notificationPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                    } else {
+                        // Set the flag that permission was denied
+                        getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit()
+                            .putBoolean(KEY_PERMISSION_DENIED_ONCE, true).apply()
+                        showPermissionDeniedDialog.value = false
+                        // Show the toast message on denial
+                        Toast.makeText(
+                            this,
+                            "You need to allow notifications to activate reminders!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
-            },
-            dismissButton = {
-                TextButton(onClick = { onActionClicked(false) }) {
-                    Text(text = "Don't ask again")
-                }
-            },
-            icon = {
-                Icon(
-                    painter = painterResource(R.drawable.alert),
-                    contentDescription = "Warning"
-                )
-            },
-            title = {
-                Text(text = "Permission Needed")
-            },
-            text = {
-                Text(text = "Enable notifications to activate reminders and stay on track with your habits!")
-            }
-        )
+            )
+        }
+
     }
+
 
     private fun createNotificationChannel() {
 
